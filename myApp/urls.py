69a93a438cc6e38d81483668051156f98ee78a8a
dawit neri <dawitneri888@gmail.com>
2023-10-27 14:46:50 +0300

    Initial commit

---
+++ myApp/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('service/', views.service, name='service'),
+    path('work/', views.work, name='work'),
+]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
---
+++ myApp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+import requests
+import json
+import csv
+import json
+import os
+
+
+def home(request):
+    return render(request, 'myApp/index.html')
+
+def about(request):
+    return render(request, 'myApp/about.html')
+
+def contact(request):
+    return render(request, 'myApp/contact.html')
+
+def service(request):
+    return render(request, 'myApp/service.html')
+
+def work(request):
+    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRTQ75ssTlfeNEXNuJ9CXV77xwdz4UAUD70chzuda7BwZhN-mZxk_FNMVmozaigGKLLPlfIbGKxTVvB/pub?output=csv"
+    response = requests.get(url)
+    csvData = response.text
+    # parse the CSV data as a list of dictionaries using the csv module
+    csvReader = csv.DictReader(csvData.splitlines())
+    jsonData = [row for row in csvReader]
+    # convert the list of dictionaries to JSON format
+    jsonString = json.dumps(jsonData)
+    #print(jsonString)
+    return render(request, 'myApp/work.html', context={"jsonString": jsonString})
