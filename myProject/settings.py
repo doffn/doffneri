12af4965a8fe27dcba78d640d6b0d5fa00521c7a
doffn <80069784+doffn@users.noreply.github.com>
2023-10-27 17:52:12 +0300

    Update settings.py

--- myProject/settings.py
+++ myProject/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@r+p2i2m+)2*@wiaf8&qk#1omu*==*-2l$+tu%^hx5h=#7ax-#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1','doffneri.vercel.app']
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
