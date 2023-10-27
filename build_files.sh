57618f02c8a42190ec2c84def415462c9623d048
doffn <80069784+doffn@users.noreply.github.com>
2023-10-28 00:55:29 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,1 @@
-pip install --upgrade pip
-pip install sqlite-devel
-pip install whitenoise
-pip install django
-pip install requests
-python3.9 manage.py collectstatic
+pip install -r requirments.txt
