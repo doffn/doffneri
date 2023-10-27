61875e6793f9053b6a51a3fac5ad40c2bb9e5902
doffn <80069784+doffn@users.noreply.github.com>
2023-10-28 01:36:50 +0300

    Update __init__.py

---
+++ myProject/__init__.py
@@ -0,0 +1,30 @@
+# List of required packages
+required_packages = [
+    ('whitenoise', 'whitenoise'),
+    ('requests', 'requests'),
+    ('django', 'django')
+    
+]
+
+# Check if a package is installed
+def package_installed(package_name):
+    try:
+        __import__(package_name)
+    except ImportError:
+        return False
+    else:
+        return True
+
+# Install a package using pip
+def install_package(package_name):
+    os.system(f"pip install {package_name}")
+
+# Import the necessary modules and packages
+for package in required_packages:
+    package_name, import_name = package
+    if package_installed(package_name):
+        globals()[import_name] = __import__(package_name)
+        #print("Packages already installed")
+    else:
+        print(f"Package {package_name} is missing. Please install it.")
+        install_package(package_name)
