61fdc80102f96d7402196a94ec61e049776db110
doffn <80069784+doffn@users.noreply.github.com>
2023-10-30 01:28:57 +0300

    Update settings.py

--- myProject/settings.py
+++ myProject/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticbuilds', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # there is some problem here
