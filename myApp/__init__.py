ea2360bd8bb1e459600fe5e19caf68cfcf23a37e
doffn <80069784+doffn@users.noreply.github.com>
2024-02-20 20:10:56 +0300

    Update __init__.py

--- myApp/__init__.py
+++ myApp/__init__.py
@@ -1,14 +1,25 @@
+import os
 
-# List of required packages
+# List of required packages 
 required_packages = [
-    ('time', 'time'),
-    ('requests', 'requests'),
-    ('os', 'os'),
     ('django', 'django'),
+    ('time', 'time'),
+    ('collections', 'Counter'),
+    ('beautifulsoup4==4.11.2', 'BeautifulSoup'),
+    ('psutil', 'psutil'),
+    ('logging', 'logging'),
+    ('random', 'random'),
+    ('threading', 'threading'),
+    ('json', 'json'),
+    ('sys', 'sys'),
+    ('telebot', 'telebot'),
+   # ('--force-reinstall urllib3==1.26.16', 'urllib3'),
+    ('pymongo', 'pymongo'),
+    ('schedule', 'schedule'),
+   # ('requests', 'requests')
 ]
 
-import os
-# Check if a package is installed
+## Check if a package is installed
 def package_installed(package_name):
     try:
         __import__(package_name)
@@ -24,11 +35,13 @@ def install_package(package_name):
 # Import the necessary modules and packages
 for package in required_packages:
     package_name, import_name = package
-    if package_installed(package_name):
-        globals()[import_name] = __import__(package_name)
-        #print("Packages already installed")
-    else:
-        print(f"Package {package_name} is missing. Please install it.")
-        install_package(package_name)
-
+    try:
+        if package_installed(package_name):
+            globals()[import_name] = __import__(package_name)
+            #print("Packages already installed")
+        else:
+            print(f"Package {package_name} is missing. Please install it.")
+            install_package(package_name)
+    except Exception as e:
+        print(e)
 
