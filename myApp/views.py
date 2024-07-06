8b53d97e8e822cb3986dedca64dfa22adfb11637
Dawit Neri <80069784+doffn@users.noreply.github.com>
2024-07-06 03:18:23 +0300

    tg

--- myApp/views.py
+++ myApp/views.py
@@ -4,6 +4,28 @@ import json
 import os
 import csv
 
+import requests
+
+def send_message_to_telegram_bot(bot_token, chat_id, message_text):
+    """
+    Sends a message to a Telegram bot.
+    
+    Args:
+        bot_token (str): The authentication token of the Telegram bot.
+        chat_id (str): The unique identifier for the chat or user to send the message to.
+        message_text (str): The text of the message to be sent.
+    
+    Returns:
+        dict: The response from the Telegram API, containing information about the sent message.
+    """
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    data = {
+        "chat_id": chat_id,
+        "text": message_text
+    }
+    response = requests.post(url, data=data)
+    return response.json()
+
 
 def home(request):
     return render(request, 'myApp/index.html')
@@ -13,6 +35,12 @@ def about(request):
 
 def contact(request):
     if request.method == 'POST':
+        bot_token = os.getenv("TOKEN")
+        chat_id = os.getenv("ID")
+        message_text = "Hello, this is a test message from my Python script!"
+
+        response = send_message_to_telegram_bot(bot_token, chat_id, message_text)
+        print(response)
         print(request.POST)
     return render(request, 'myApp/contact.html')
 
